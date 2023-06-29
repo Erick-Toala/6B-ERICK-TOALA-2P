ab02b3b7cf407490ef77cc14f8b63d2d596f0198
Erick Toala <94879384+Erick-Toala@users.noreply.github.com>
2023-06-29 00:04:16 -0500

    Add files via upload

---
+++ practica2/06-layers/controllers/index.js
@@ -0,0 +1,9 @@
+const Product =  require('./product');
+const Category =  require('./category');
+const Deportista = require('./deportista')
+
+module.exports= {
+    Deportista,
+    Product,
+    Category
+}
---
+++ practica2/06-layers/models/deportista.js
@@ -0,0 +1,35 @@
+const { model, Schema } = require('mongoose');
+
+const DeportistaSchema = Schema(
+    {
+        id:{
+            type: Number,
+            required: true,
+            unique: true
+        },
+        nombre:{
+            type: String,
+            required: [ true, 'El nombre del Deportista es necesario'],
+            unique:true
+        },
+        peso:{
+            type: Number,
+            default:0
+        },
+        altura:{
+            type:Number,
+            default:0
+        },
+        edad:{
+            type:Number,
+            default:0
+        }
+    }
+);
+
+DeportistaSchema.methods.toJSON = function(){
+    const { __v,  status,  ...data   } =  this.toObject();
+    return data;
+}
+
+module.exports = model('Deportista', DeportistaSchema );
